d46e03ac70f1bb834997608f0cb2641c29419e2d
Marcelo Felix Salgado <marcelo-felix-luizalabs@WMEC-P2CCPXX3FC.local>
2023-04-04 12:03:23 -0300

    Creating a common settings file

--- settings/settings.go
+++ settings/settings.go
@@ -1,14 +1,6 @@
 package settings
 
-import (
-	"log"
-
-	"github.com/codingconcepts/env"
-	"github.com/joho/godotenv"
-	"github.com/sirupsen/logrus"
-)
-
-// ConfigType struct to resolve env vars
+// Common ConfigType struct to resolve env vars
 type ConfigType struct {
 	AppName     string `env:"APP_NAME" default:"financial-api-response-module"`
 	Environment string `env:"ENVIRONMENT" default:"development"`
@@ -35,22 +27,3 @@ type ConfigType struct {
 }
 
 var Config ConfigType
-
-// InitConfigs initializes the environment settings
-func Load() {
-	// load .env (if exists)
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Println("No .env file found")
-	}
-
-	// bind env vars
-	if err := env.Set(&Config); err != nil {
-		log.Fatal(err)
-	}
-
-	if _, err := logrus.ParseLevel(Config.LogLevel); err != nil {
-		log.Fatal(err)
-	}
-}
