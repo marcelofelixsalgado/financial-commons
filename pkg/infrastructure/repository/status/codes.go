8faea4752abfe5e9f02ff174c3b4840d71a2b7f7
Marcelo Felix Salgado <marcelo-felix-luizalabs@WMEC-P2CCPXX3FC.local>
2023-04-04 22:56:09 -0300

    Added OverlappingPeriodDates repository internal status

--- pkg/infrastructure/repository/status/codes.go
+++ pkg/infrastructure/repository/status/codes.go
@@ -6,4 +6,5 @@ const (
 	Success                        RepositoryInternalStatus = "OK"
 	InternalServerError            RepositoryInternalStatus = "INVALID_CONTENT_TYPE"
 	EntityWithSameKeyAlreadyExists RepositoryInternalStatus = "ENTITY_WITH_SAME_KEY_ALREADY_EXISTS"
+	OverlappingPeriodDates         RepositoryInternalStatus = "OVERLAPPING_PERIOD_DATES"
 )
