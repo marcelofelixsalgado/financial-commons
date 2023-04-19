7b84d8f0df9dc5129568c6207cd019919706d8a3
Marcelo Felix Salgado <marcelo-felix-luizalabs@WMEC-P2CCPXX3FC.local>
2023-04-19 14:10:22 -0300

    Ignoring .env file in production environments

--- settings/settings.go
+++ settings/settings.go
@@ -2,6 +2,7 @@ package settings
 
 import (
 	"log"
+	"os"
 
 	"github.com/codingconcepts/env"
 	"github.com/joho/godotenv"
@@ -40,10 +41,11 @@ var Config ConfigType
 func Load() {
 
 	// load .env (if exists)
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("No .env file found")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Println("No .env file found")
+	// }
+	loadDotEnv()
 
 	// bind env vars
 	if err := env.Set(&Config); err != nil {
@@ -52,5 +54,14 @@ func Load() {
 
 	if _, err := logrus.ParseLevel(Config.LogLevel); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func loadDotEnv() {
+	if _, err := os.Stat(".env"); err == nil {
+		if err := godotenv.Load(); err != nil {
+			logrus.Fatal("Error loading .env file")
+		}
+		logrus.Println("Using .env file")
 	}
 }
